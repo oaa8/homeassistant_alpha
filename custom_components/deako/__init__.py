724604057d0500479525efe639e2739372a18a32
Tola Adeola <Tola@microsoft.com>
2023-12-29 13:27:37 -0800

    Support for custom IP address

--- custom_components/deako/__init__.py
+++ custom_components/deako/__init__.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 
 import logging
 
-from pydeako.deako import Deako, FindDevicesTimeout
+from pydeako.deako import Deako, DeviceListTimeout, FindDevicesTimeout
 from pydeako.discover import DeakoDiscoverer
 
 from homeassistant.components import zeroconf
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
@@ -21,35 +21,9 @@ PLATFORMS: list[Platform] = [Platform.LIGHT]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up deako from a config entry."""
-    hass_data = hass.data.setdefault(DOMAIN, {})
-    if hass_data is None or not isinstance(hass_data, dict):
-        hass_data = {}
-        hass.data[DOMAIN] = hass_data
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
-    if hass_data.get(DISCOVERER_ID) is None:
-        _zc = await zeroconf.async_get_instance(hass)
-        _dd = DeakoDiscoverer(_zc)
-        hass_data[DISCOVERER_ID] = _dd
-
-    discoverer: DeakoDiscoverer = hass_data.get(DISCOVERER_ID)
-
-    connection = Deako(discoverer.get_address)
-    await connection.connect()
-    try:
-        await connection.find_devices()
-    except FindDevicesTimeout as exc:
-        _LOGGER.warning("No devices expected")
-        await connection.disconnect()
-        raise ConfigEntryNotReady(exc) from exc
-
-    devices = connection.get_devices()
-    if len(devices) == 0:
-        await connection.disconnect()
-        raise ConfigEntryNotReady(devices)
-
-    hass.data[DOMAIN][entry.entry_id] = connection
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await _initiate_connection(hass, entry)
 
     return True
 
@@ -62,3 +36,68 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Update Deako listeners."""
+
+    connection = hass.data.get(DOMAIN, {}).get(entry.entry_id)
+    if connection is not None:
+        hass.data[DOMAIN].pop(entry.entry_id)
+        await connection.disconnect()
+
+    await _initiate_connection(hass, entry)
+
+
+async def _initiate_connection(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Initiate Deako connection."""
+    hass_data = hass.data.setdefault(DOMAIN, {})
+    if hass_data is None or not isinstance(hass_data, dict):
+        hass_data = {}
+        hass.data[DOMAIN] = hass_data
+
+    if entry.options is not None:
+
+        async def get_address_method() -> str:
+            return (
+                f"{entry.options.get(CONF_IP_ADDRESS)}:{entry.options.get(CONF_PORT)}"
+            )
+
+        get_address = get_address_method
+
+    elif entry.data is not None:
+
+        async def get_address_method() -> str:
+            return f"{entry.data.get(CONF_IP_ADDRESS)}:{entry.data.get(CONF_PORT)}"
+
+        get_address = get_address_method
+    else:
+        if hass_data.get(DISCOVERER_ID) is None:
+            _zc = await zeroconf.async_get_instance(hass)
+            _dd = DeakoDiscoverer(_zc)
+            hass_data[DISCOVERER_ID] = _dd
+
+        discoverer: DeakoDiscoverer = hass_data.get(DISCOVERER_ID)
+        get_address = discoverer.get_address
+
+    connection = Deako(get_address)
+    await connection.connect()
+    try:
+        await connection.find_devices()
+    except FindDevicesTimeout as exc:
+        _LOGGER.warning("No devices expected")
+        await connection.disconnect()
+        raise ConfigEntryNotReady(exc) from exc
+    except DeviceListTimeout as exc:
+        _LOGGER.warning("No devices expected")
+        await connection.disconnect()
+        raise ConfigEntryNotReady(exc) from exc
+
+    devices = connection.get_devices()
+    if len(devices) == 0:
+        await connection.disconnect()
+        raise ConfigEntryNotReady(devices)
+
+    hass.data[DOMAIN][entry.entry_id] = connection
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
--- custom_components/deako/config_flow.py
+++ custom_components/deako/config_flow.py
@@ -1,26 +1,125 @@
 """Config flow for deako."""
 
-from pydeako.discover import DeakoDiscoverer, DevicesNotFoundException
+import logging
+from typing import Any
 
+from pydeako.discover import DeakoDiscoverer, DevicesNotFoundException
+import voluptuous as vol
+
+from homeassistant import config_entries
 from homeassistant.components import zeroconf
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import config_entry_flow
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN, NAME
 
-
-async def _async_has_devices(hass: HomeAssistant) -> bool:
-    """Return if there are devices that can be discovered."""
-    _zc = await zeroconf.async_get_instance(hass)
-    discoverer = DeakoDiscoverer(_zc)
-
-    try:
-        await discoverer.get_address()
-        # address exists, there's at least one device
-        return True
-
-    except DevicesNotFoundException:
-        return False
+_LOGGER = logging.getLogger(__name__)
 
 
-config_entry_flow.register_discovery_flow(DOMAIN, NAME, _async_has_devices)
+class DeakoOptionsFlowHandler(config_entries.OptionsFlow):
+    """Deako config flow options handler."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize HACS options flow."""
+        self.config_entry = config_entry
+        self.options = dict(config_entry.options)
+        # self.discovered_devices = None
+
+    # async def async_step_init(
+    #         self, _user_input: dict[str, Any] | None = None, errors: dict[str, str] = None
+    # ) -> FlowResult:  # pylint: disable=unused-argument
+    #     """Manage the options."""
+    #     return await self.async_step_user(_user_input, errors)
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None, errors: dict[str, str] = None
+    ) -> FlowResult:
+        """Handle a flow initialized by the user."""
+        # return self.async_show_menu(
+        #     step_id="user", menu_options=[CONF_IPV4, CONF_PORT]
+        # )
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Deako",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_IP_ADDRESS,
+                        default=self.config_entry.options.get(
+                            CONF_IP_ADDRESS, "192.168.86.46"
+                        ),
+                    ): str,
+                    vol.Required(
+                        CONF_PORT, default=self.config_entry.options.get(CONF_PORT, 23)
+                    ): int,
+                }
+            ),
+            errors=errors,
+        )
+
+
+class DeakoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Deako."""
+
+    VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None, errors: dict[str, str] = None
+    ) -> FlowResult:
+        """Handle the user step of the config flow."""
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Deako",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_IP_ADDRESS, default="192.168.86.46"): str,
+                    vol.Required(CONF_PORT, default=23): int,
+                }
+            ),
+            errors=errors,
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> DeakoOptionsFlowHandler:
+        """Get the options flow for this handler."""
+        return DeakoOptionsFlowHandler(config_entry)
+
+
+# async def _async_has_devices(hass: HomeAssistant) -> bool:
+#     """Return if there are devices that can be discovered."""
+#     _zc = await zeroconf.async_get_instance(hass)
+#     discoverer = DeakoDiscoverer(_zc)
+
+#     _LOGGER.error("Deako config_flow.py _async_has_devices")
+#     try:
+#         _LOGGER.error("Deako config_flow.py _async_has_devices try")
+#         await discoverer.get_address()
+#         _LOGGER.error("Deako config_flow.py _async_has_devices try await")
+#         # address exists, there's at least one device
+#         return True
+
+#     except DevicesNotFoundException:
+#         _LOGGER.error("Deako config_flow.py _async_has_devices except")
+#         return False
+
+
+# config_entry_flow.register_discovery_flow(DOMAIN, NAME, _async_has_devices)
