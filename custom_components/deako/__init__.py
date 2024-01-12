8d08b33dc0046e746aa8fd22107ed2a7b3633ee5
Tola Adeola <Tola@microsoft.com>
2024-01-11 20:25:19 -0800

    Add protection for empty space characters flooding the telnet stream and also reload the integration if more than 1000 whitespace only messages have been received in a row to avoid taking down home assistant

--- custom_components/deako/__init__.py
+++ custom_components/deako/__init__.py
@@ -1,6 +1,7 @@
 """The deako integration."""
 from __future__ import annotations
 
+import asyncio
 import logging
 from typing import Callable
 
@@ -178,6 +179,7 @@ async def _initiate_connection(hass: HomeAssistant, entry: ConfigEntry) -> None:
     set_delay_options_if_needed(hass, entry)
 
     connection = Deako(get_address)
+
     if is_address_hardcoded:
         connection.__is_address_hardcoded = True
         connection.__address = await get_address()
@@ -203,6 +205,54 @@ async def _initiate_connection(hass: HomeAssistant, entry: ConfigEntry) -> None:
     connection.is_refreshing.store(ATOMIC_BOOL_FALSE)
     connection.is_additional_refresh_requested = atomics.atomic(1, atomics.INT)
     connection.is_additional_refresh_requested.store(ATOMIC_BOOL_FALSE)
+    overwrite_parse_data_implementation(hass, entry, connection)
     hass.data[DOMAIN][entry.entry_id] = connection
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+
+def overwrite_parse_data_implementation(
+    hass: HomeAssistant, entry: ConfigEntry, connection: Deako
+) -> None:
+    """Overwrite the parse_data implementation."""
+
+    def new_parse_data(self, data: bytes) -> None:
+        """Replacement parse_data method that replaces self.message_buffer with an empty string if it only contains whitespace before calling the original implementation."""
+
+        if (
+            (self.message_buffer is not None)
+            and (self.message_buffer.strip() == "")
+            and len(self.message_buffer) > 0
+        ):
+            _LOGGER.info(
+                "Replacing message buffer with empty string after seeing %s whitespace characters",
+                len(self.message_buffer),
+            )
+            self.message_buffer = ""
+
+        raw_string = data.decode("utf-8")
+        if (raw_string is not None) and (raw_string.strip() == ""):
+            if self.empty_message_counter is None:
+                self.empty_message_counter = 0
+            self.empty_message_counter += 1
+            if self.empty_message_counter > 1000:
+                _LOGGER.error(
+                    "Received %s empty messages in a row.  Reloading integration to reconnect",
+                    self.empty_message_counter,
+                )
+                asyncio.ensure_future(hass.config_entries.async_reload(entry.entry_id))  # noqa: RUF006
+        else:
+            self.empty_message_counter = 0
+        pydeako.deako.utils._connection._Connection.__old_parse_data(self, data)
+
+    _LOGGER.info(
+        "Reassigning _Connection's implementation of parse_data to protect against streams of whitespace"
+    )
+
+    # This is to protect against what appeared to be a stream of whitespace characters that took the entire home assistant instance down
+    # An additional protective method would be to reconnect once a certain number of whitespace characters are seen in a row with an empty buffer.
+    if not hasattr(pydeako.deako.utils._connection._Connection, "__old_parse_data"):
+        pydeako.deako.utils._connection._Connection.__old_parse_data = (
+            pydeako.deako.utils._connection._Connection.parse_data
+        )
+        pydeako.deako.utils._connection._Connection.parse_data = new_parse_data
